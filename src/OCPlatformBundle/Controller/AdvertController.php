61e684c96eb9bf754d3b12cfba828b4296da2af8
Tchavanne <tch.asdoria@gmail.com>
2018-07-04 08:47:19 +0200

    Base Routing Templating Controllers

--- src/OCPlatformBundle/Controller/AdvertController.php
+++ src/OCPlatformBundle/Controller/AdvertController.php
@@ -6,9 +6,9 @@ namespace OCPlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 /**
  * Class AdvertController
@@ -17,34 +17,123 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 class AdvertController extends Controller
 {
     /**
+     * @param $page
+     *
      * @return Response
      */
-    public function indexAction(): Response
+    public function indexAction($page): response
     {
 
-        $url  = $this->get('router')->generate(
-            'oc_platform_view',
-            array('id' => 5),
-            UrlGeneratorInterface::ABSOLUTE_URL
-        );
-        $trad = $this->get('translator')->trans('asdoria.ui.controller.advert.index'
-        );
+        if ($page < 1) {
 
-        return new Response($trad . $url);
+            throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
+
+        }
+
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => array()));
     }
 
+    /**
+     * @param $id
+     *
+     * @return Response
+     */
+    public function viewAction(int $id): response
+    {
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function addAction(Request $request): response
+    {
+
+        if ($request->isMethod('POST')) {
+
+            $this->addFlash('notice', 'Annonce bien enregistrée.');
+
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+
+        }
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+
+    }
+
+    /**
+     * @param int     $id
+     * @param Request $request
+     *
+     * @return Response
+     */
+
+    public function editAction(int $id, Request $request): response
+    {
+
+        $advert = [
+
+            'title' => 'Recherche développpeur Symfony',
+
+            'id' => $id,
+
+            'author' => 'Alexandre',
+
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+
+            'date' => new \Datetime()
+
+        ];
+
+
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
+
+            'advert' => $advert
+
+        ));
+
+    }
 
     /**
      * @param int $id
      *
      * @return Response
      */
-    public function viewAction(int $id): Response
+    public function deleteAction(int $id): response
     {
 
-        return $this->redirectToRoute('oc_platform_home');
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
 
     }
 
+    /**
+     * @param $limit
+     *
+     * @return Response
+     */
+    public function menuAction($limit): response
+    {
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
 
+        return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
+    }
 }
